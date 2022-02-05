27163601b4738edc2f4ee8b267fe02c13fdcf185
chenenjie <785019257@qq.com>
2022-02-05 11:12:20 +0800

    解析jwt的内容

--- src/main/java/com/example/springsecurityoauth2demo/controller/UserController.java
+++ src/main/java/com/example/springsecurityoauth2demo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.example.springsecurityoauth2demo.controller;
 
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("user")
@@ -16,7 +20,13 @@ public class UserController {
      * @return
      */
     @GetMapping("getCurrentUser")
-    public Object getCurrentUser(Authentication authentication){
-        return authentication.getPrincipal();
+    public Object getCurrentUser(Authentication authentication, HttpServletRequest request){
+        String header = request.getHeader("Authorization");
+        String token = header.substring(header.indexOf("bear") + 7);
+        return Jwts.parser()
+                .setSigningKey("test_key".getBytes(StandardCharsets.UTF_8))
+                .parseClaimsJws(token)
+                .getBody();
+//        return authentication.getPrincipal();
     }
 }
